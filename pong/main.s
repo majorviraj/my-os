82d9e15b02ea6ea7cdaeaf7b4c9a84939331bd89
majorviraj <majorviraj@gmail.com>
2018-01-23 00:48:47 +0530

    Debugged and tested, working now with minor problems and scope for optimization. Problems- -Flickering due to erasing ball & paddle at previous place -USB driver detecting false press and moving paddle1 down continuously -Score not printed on screen -Splashscreen pending

--- pong/main.s
+++ pong/main.s
@@ -13,53 +13,50 @@
 */
 
 .section .data
-circleBitmap:
-	.rept 200
-	.byte 0
-	.endr
-.align 2
+
+.align 1
 ballX:
 	.hword 512
-.align 2
+.align 1
 ballY:
 	.hword 512
-.align 2
+.align 1
 speedX:
-	.hword 10
-.align 2
+	.hword 3
+.align 1
 speedY:
-	.hword 10
-.align 2
+	.hword 3
+.align 1
 paddle1Y:
-	.hword 40
-.align 2
+	.hword 300
+.align 1
 paddle2Y:
-	.hword 40
-.align 2
+	.hword 550
+.align 1
 paddle1Length:
 	.hword 80
-.align 2
+.align 1
 paddle2Length:
 	.hword 80
-.align 2
+.align 1
 radiusOfBall:
 	.hword 20
-.align 2
+.align 1
 MaxYValue:
 	.hword 700
-.align 2
+.align 1
 MinYValue:
 	.hword 100
-.align 2
+.align 1
 paddle1RightmostX:
 	.hword 180
-.align 2
+.align 1
 paddle1LeftmostX:
 	.hword 150
-.align 2
+.align 1
 paddle2RightmostX:
 	.hword 1000
-.align 2
+.align 1
 paddle2LeftmostX:
 	.hword 970
 
@@ -73,10 +70,13 @@ p2Score:
 _start:
 	b main
 
+.section .text
 main:
+
 	mov sp, #0x8000			@Initialise the stack at 0x8000
 							@as the rpi bootloader is setup in such a way
-	ldr r0, =#1024			@width
+	
+	mov r0, #1024			@width
 	mov r1, #768			@height
 	mov r2, #16				@bitDepth
 	bl frameBufferInit
@@ -84,12 +84,12 @@ main:
 	teq r0, #0
 	bne noError$
 
-	mov r0,#47
-	mov r1,#1
+	mov r0, #47
+	mov r1, #1
 	bl SetGpioFunction
 
-	mov r0,#47
-	mov r1,#0
+	mov r0, #47
+	mov r1, #0
 	bl SetGpio
 
 	error$:
@@ -101,8 +101,70 @@ main:
 
 	bl UsbInitialise
 
+	ldr r0,= 0xFFFF
+	bl setForeColour
+
+	ldr r0,=#35285
+	bl setForeColour
+	bl FullScreenToForeColour
+
+	ldr r0, = #5000000
+	bl delayMicro
+
+	ldr r0,=0xFFFF
+	bl setForeColour
+
+	bl FullScreenToForeColour
+	ldr r0, = #5000000
+	bl delayMicro
+
+	
+
 
 gameLoop$:
+
+	@Erase previous ball and paddle traces!
+	ldr r0,= 0xFFFF
+	bl setForeColour
+
+	@Draw left paddle1 here
+	ldr r0,= paddle1LeftmostX
+	ldrh r0, [r0]
+	ldr r1,= paddle1Y
+	ldrh r1, [r1]
+	ldr r4,=paddle1Length
+	ldrh r4, [r4]
+	ldr r2,= paddle1RightmostX
+	ldrh r2, [r2]
+	add r3, r1, r4
+	bl drawRectangle
+
+	@Draw right paddle2 here
+	ldr r0,= paddle2LeftmostX
+	ldrh r0, [r0]
+	ldr r1,= paddle2Y
+	ldrh r1, [r1]
+	ldr r4,=paddle2Length
+	ldrh r4, [r4]
+	ldr r2,= paddle2RightmostX
+	ldrh r2, [r2]
+	add r3, r1, r4
+	bl drawRectangle
+
+	@draw circle
+	ldr r0,=ballX
+	ldrh r0, [r0]
+	ldr r1,=ballY
+	ldrh r1, [r1]
+	ldr r2,=radiusOfBall
+	ldrh r2, [r2]
+	bl drawFilledCircle
+
+
+
+
+	
+	
 	mov r0, #1
 	bl checkInputs			@Takes r0 as input, r0=1 is P1 and r0=2 is P2
 							@Return into r0, up arraw and into r1, down arrow state
@@ -110,27 +172,25 @@ gameLoop$:
 	@Update paddle1Y according to inputs
 	cmp r0, #0	@check if P1 up array is pressed
 	ldreq r2,=paddle1Y
-	ldreq r3,[r2]
-	addeq r3, #1
-	streq r3, [r2]
+	ldreqh r3,[r2]
+	addeq r3, #10
+	streqh r3, [r2]
 	@check if paddle1Y exceeds max possible value
 	ldr r5, =MaxYValue
-	ldr r5, [r5]
+	ldrh r5, [r5]
 	cmp r3, r5
-	movhi r4, r5
-	strhi r4, [r2]
+	strhih r5, [r2]
 
 	cmp r1, #1	@check if P1 down array is pressed
 	ldreq r2,=paddle1Y
-	ldreq r3,[r2]
-	subeq r3, #1
-	streq r3, [r2]
+	ldreqh r3,[r2]
+	subeq r3, #10
+	streqh r3, [r2]
 	@check if paddle1Y exceeds min possible value
 	ldr r5, =MinYValue
-	ldr r5, [r5]
+	ldrh r5, [r5]
 	cmp r3, r5
-	movls r4, r5
-	strls r4, [r2]
+	strlsh r5, [r2]
 
 
 	@Update paddle2Y according to inputs
@@ -139,65 +199,92 @@ gameLoop$:
 
 	cmp r0, #0	@check if P2 up array is pressed
 	ldreq r2,=paddle2Y
-	ldreq r3,[r2]
-	addeq r3, #1
-	streq r3, [r2]
+	ldreqh r3,[r2]
+	addeq r3, #10
+	streqh r3, [r2]
 	@check if paddle2Y exceeds max possible value
 	ldr r5, =MaxYValue
-	ldr r5, [r5]
+	ldrh r5, [r5]
 	cmp r3, r5
-	movhi r4, r5
-	strhi r4, [r2]
+	strhih r5, [r2]
 
 	cmp r1, #1	@check if P2 down array is pressed
 	ldreq r2,=paddle2Y
-	ldreq r3,[r2]
-	subeq r3, #1
-	streq r3,[r2]
+	ldreqh r3,[r2]
+	subeq r3, #10
+	streqh r3,[r2]
 	@check if paddle2Y exceeds min possible value
 	ldr r5, =MinYValue
-	ldr r5, [r5]
+	ldrh r5, [r5]
 	cmp r3, r5
-	movls r4, r5
-	strls r4, [r2]
+	strlsh r5, [r2]
 
+	
 
 	@Update coordinates of ball by speedX and speedY
 	ldr r2,=ballX
-	ldr r4, [r2]
+	ldrh r4, [r2]
 	ldr r3,=speedX
-	ldr r3, [r3]
+	ldrh r3, [r3]
 	add r5, r4, r3
-	str r5, [r2]
+	strh r5, [r2]
+
 
 	ldr r2,=ballY
-	ldr r4, [r2]
-	ldr r3,=speedY
-	ldr r3, [r3]
-	add r5, r4, r3
-	str r5, [r2]
+	ldrh r3, [r2]
+	ldr r4,=speedY
+	ldrh r4, [r4]
+	add r5, r3, r4
+	strh r5, [r2]
 
 	@Limit ballX and ballY within limits here
+	ldr r2,=ballY
+	ldrh r3, [r2]
+	ldr r4,= MinYValue
+	ldrh r4, [r4]
+	ldr r5,= MaxYValue
+	ldrh r5, [r5]
+	cmp r3, r4
+	strleh r4, [r2]
+	cmp r3, r5
+	strgeh r5, [r2]	
 
-
-
+	ldr r2,=ballX
+	ldrh r3, [r2]
+	ldr r4,=#0
+	ldr r5,=#1024 
+	cmp r3, r4
+	strleh r4, [r2]
+	cmp r3, r5
+	strgeh r5, [r2]
 
 
 	@Change speedY according to ballY
+	
+	/*if (bally>maxY | ballY<minY) {
+		negate speedy
+	}
+	*/
 	ldr r2,=ballY
-	ldr r3, [r2]
+	ldrh r3, [r2]
 	ldr r4,=speedY
-	ldr r5, [r4]
-	ldr r6, =MaxYValue
-	ldr r6, [r6]
+	ldrh r5, [r4]
+	ldr r6,=MinYValue
+	ldrh r6, [r6]
+	ldr r7,=MaxYValue
+	ldrh r7, [r7]
 	cmp r3, r6
-	ldr r6, =MaxYValue
-	ldr r6, [r6]
-	cmple r3, r6
-	bge skipChangingYSpeed$
-	neg r5,r5
-	str r5, [r4]
-	skipChangingYSpeed$:
+	ble negate$
+	cmpge r3, r7
+	bge negate$
+
+	b skipChangingYSpeed$
+
+	negate$:
+	neg r5, r5
+	strh r5, [r4]
+
+skipChangingYSpeed$:
 
 	@Change speedX according to ballY, paddle1Y & paddle2Y only if ballX = paddle1X or paddle2X
 	ldr r3,=ballX
@@ -207,193 +294,133 @@ gameLoop$:
 	ldr r7,=paddle1Y
 	ldr r8,=paddle2Y
 	ldr r9,=radiusOfBall
-	ldr r9, [r9]
+	ldrh r9, [r9]
 	ldr r10,=paddle1Length
-	ldr r10, [r10]
+	ldrh r10, [r10]
 	ldr r11,=paddle2Length
-	ldr r11, [r11]
+	ldrh r11, [r11]
 
 	@Check if ballX is away from paddl1X and paddle2X
-	ldr r0, [r3]
+	ldrh r0, [r3]
 	sub r1, r0, r9	@done to evaluate leftmost ball coordinate
 
  	ldr r2,= paddle1RightmostX
- 	ldr r2, [r2]
+ 	ldrh r2, [r2]
 	cmp r1, r2
-	addne r1, r0, r9
-	ldrne r2,= paddle2LeftmostX
-	ldrne r2, [r2]
-	cmpne r1, r2
-	bne ballNotNearPaddles$
+	ble checkForPaddle1$
+	add r1, r0, r9
+	ldr r2,= paddle2LeftmostX
+	ldrh r2, [r2]
+	cmp r1, r2
+	bge checkForPaddle2$
+
+	b ballNotNearPaddles$
 
 
-		@Check if ballX is near paddle1X
-		ldr r0, [r3]
-		sub r1, r0, r9 @done to evaluate leftmost ball coordinate
-		ldr r2,= paddle1RightmostX
-		ldr r2, [r2]
-		teq r1, r2
-		bne checkForPaddle2$
-		ldr r0, [r4]	@ball is at paddle1, do whatever necessary below
-		ldr r1, [r7]
-		add r2, r1, r10
+
+	checkForPaddle1$:
+		ldrh r0, [r4]	@ball is at paddle1, do whatever necessary below
+		ldrh r1, [r7]	@paddle1Y
+		add r2, r1, r10	@paddle1Y + paddle1Length
 		cmp r0, r1
-		cmpge r0, r2
-		ldrle r2, [r5]	@negate speedX as ball as hit paddle
-		negle r2, r2
-		strle r2, [r5]
-		ble checkForPaddle2$
+		blt ballNotOnPaddle1$
+		cmp r0, r2
+		bgt ballNotOnPaddle1$
+		ldrh r2, [r5]	@negate speedX as ball as hit paddle
+		neg r2, r2
+		strh r2, [r5]
+		b ballNotNearPaddles$
+
+		ballNotOnPaddle1$:
 		ldr r1,= p2Score
-		ldr r2, [r1]
+		ldrb r2, [r1]
 		add r2, #1
-		str r2, [r1]
+		strb r2, [r1]
 		@reset ballX ballY to center
-		ldr r2,= #512 @Needs to be changed
-		str r2, [r3]
-		str r2, [r4]
+		ldr r1,= #512 @Needs to be changed, hardcoded currently
+		ldr r2,= #400 @Needs to be changed, hardcoded currently
+		strh r1, [r3]
+		strh r2, [r4]
+		ldrh r2, [r5] @negate speedX, to change intitial ball direction
+		neg r2, r2
+		strh r2, [r5]
+		b ballNotNearPaddles$
 
 
 	@check if ballX is near paddle2X
 	checkForPaddle2$:
-		ldr r0, [r3]	@load ballX
-		add r1, r0, r9 @done to evaluate rightmost ball coordinate
-		ldr r2,= paddle2LeftmostX
-		teq r1, r2
-		bne ballNotNearPaddles$
-		ldr r0, [r4]	@load ballY @ball is at paddle2, do whatever necessary below
-		ldr r1, [r8]
-		add r2, r1, r11
+		ldrh r0, [r4]	@ballY @load ballY @ball is at paddle2, do whatever necessary below
+		ldrh r1, [r8]	@paddle2Y
+		add r2, r1, r11 @paddle2Y+ paddle2Length
 		cmp r0, r1
-		cmpge r0, r2
-		ldrle r2, [r5] @negate speedX as ball as hit paddle
-		negle r2, r2
-		strle r2, [r5]
-		ble ballNotNearPaddles$
-		ldr r1,= p1Score
-		ldr r2, [r1]
-		add r2, #1
-		str r2, [r1]
-		@reset ballX ballY to center
-		ldr r2,= #512 @Needs to be changed, hardcoded currently
-		str r2, [r3]
-		str r2, [r4]
+		blt ballNotOnPaddle2$
+		cmp r0, r2
+		bgt ballNotOnPaddle2$
+		ldrh r2, [r5] @negate speedX as ball as hit paddle
+		neg r2, r2
+		strh r2, [r5]
+		b ballNotNearPaddles$
+		
+		ballNotOnPaddle2$:
+			ldr r1,= p1Score
+			ldrb r2, [r1]
+			add r2, #1
+			strb r2, [r1]
+			@reset ballX ballY to center
+			ldr r1,= #512 @Needs to be changed, hardcoded currently
+			ldr r2,= #400 @Needs to be changed, hardcoded currently
+			strh r1, [r3]
+			strh r2, [r4]
+			ldrh r2, [r5] @negate speedX, to change intitial ball direction
+			neg r2, r2
+			strh r2, [r5]
 
-
+	
 	ballNotNearPaddles$:
 
-	@Render all the graphics, paddles, ball and p1Score,p2Score
+
+		@Render all the graphics, paddles, ball and p1Score,p2Score
+
+	ldr r0,=#0
+	bl setForeColour
 
 	@Draw left paddle1 here
 	ldr r0,= paddle1LeftmostX
-	ldr r0, [r0]
+	ldrh r0, [r0]
 	ldr r1,= paddle1Y
-	ldr r1, [r1]
+	ldrh r1, [r1]
 	ldr r4,=paddle1Length
-	ldr r4, [r4]
+	ldrh r4, [r4]
 	ldr r2,= paddle1RightmostX
-	ldr r2, [r2]
+	ldrh r2, [r2]
 	add r3, r1, r4
 	bl drawRectangle
 
 	@Draw right paddle2 here
 	ldr r0,= paddle2LeftmostX
-	ldr r0, [r0]
+	ldrh r0, [r0]
 	ldr r1,= paddle2Y
-	ldr r1, [r1]
+	ldrh r1, [r1]
 	ldr r4,=paddle2Length
-	ldr r4, [r4]
+	ldrh r4, [r4]
 	ldr r2,= paddle2RightmostX
-	ldr r2, [r2]
+	ldrh r2, [r2]
 	add r3, r1, r4
 	bl drawRectangle
 
 	@draw circle
+	ldr r0,=ballX
+	ldrh r0, [r0]
+	ldr r1,=ballY
+	ldrh r1, [r1]
+	ldr r2,=radiusOfBall
+	ldrh r2, [r2]
+	bl drawFilledCircle
 
+	@ldr r0, = #10000
+	@bl delayMicro
 	b gameLoop$
 
-.globl checkInputs
-checkInputs:
-
-	cmp r0, #3
-	movge pc, lr
-
-	push {r4,r5,r6,lr}
-
-	teq r0, #1
-	beq player1loop$
-
-	teq r0, #2
-	beq player2loop$
-
-player1loop$:
-
-	bl keyboardInit
-
-	ldr r0,=keyboardAddress
-	teq r0,#0
-	popeq {r4,r5,r6,pc}
-
-	ldr r1,[r0]
-	teq r1,#0
-	beq player1loop$
-
-	mov r4,r1
-	mov r5,#0
-
-	keyLoop$:
-		
-		ldr r0, =keyboardOldDown
-		ldrh r0, [r0]
-
-		teq r0, #0x1a
-		moveq r0, #1
-		moveq r1, #0
-		popeq {r4,r5,r6,pc}
-
-		teq r0, #0x16
-		moveq r0, #0
-		moveq r1, #1
-		popeq {r4,r5,r6,pc}
-
-		add r5,#1
-		cmp r5,#6
-		blt keyLoop$
-
-		pop {r4,r5,r6,pc}
-
-player2loop$:
-	bl keyboardInit
-
-	ldr r0,=keyboardAddress
-	teq r0,#0
-	beq player1loop$
-
-	ldr r1,[r0]
-	teq r1,#0
-	beq player1loop$
-
-	mov r4,r1
-	mov r5,#0
-
-	keyLoop2$:
-		ldr r0, =keyboardOldDown
-		ldrh r0, [r0]
-
-		teq r0, #0x52
-		moveq r0, #1
-		moveq r1, #0
-		popeq {r4,r5,r6,pc}
-
-		teq r0, #0x51
-		moveq r0, #0
-		moveq r1, #1
-		popeq {r4,r5,r6,pc}
-
-		add r5,#1
-		cmp r5,#6
-		blt keyLoop2$
-
-		pop {r4,r5,r6,pc}
 /*
 .globl renderFilledCircleToMemory
 renderFilledCircleToMemory:
