47d46c920f82bd50e8aab973bd7eafdb9c797f8a
chinmaykhopde <chinmaykhopde@gmail.com>
2018-01-24 01:01:45 +0530

    Interrupt vector table written not tested

--- libraries/cStartup.s
+++ libraries/cStartup.s
@@ -3,9 +3,36 @@
 .global _start
 _start:
 
+    b _reset_
+    b undefinedInstruction
+    b softwareInterrupt
+    b prefetchAbort
+    b _reset_
+    b _reset_
+    b interruptReq
+    b fastInterrupt
+
+    b _inf_loop
+
+_reset_:
+    mov r0, #0x8000
+    mov r1, #0x0000
+    ldmia r0!, {r2,r3,r4,r5,r6,r7,r8,r9}
+    stmia r1!, {r2,r3,r4,r5,r6,r7,r8,r9}
+    
     mov sp, #0x8000
 
     b _bss_clear
+
+fastInterrupt:
+
+interruptReq:
+
+undefinedInstruction:
+
+softwareInterrupt:
+
+prefetchAbort:
 
 .globl _bss_clear
 _bss_clear:
