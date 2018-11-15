36a84a752a655496b8a52ddff32791a202f5820a
majorviraj <majorviraj@gmail.com>
2018-11-15 16:44:58 +0530

    Final UART commit. Everything tested. Also updated the emptyCodeTemplate folder, it has everything you need in life.

--- C_Codes/uart/source/uart.c
+++ C_Codes/uart/source/uart.c
@@ -1,6 +1,7 @@
 #include <stdOutput.h>
 #include <stdInput.h>
 #include <uart.h>
+#include <timer.h>
 
 int hh;
 void frameBufferSetup(int width, int height, int bitDepth)
@@ -24,6 +25,10 @@ void kernel_main()
     uart_init();
     _enable_interrupts();
 
+    //To toggle GPIO
+    setLEDasOutput();
+    timerInit(LOAD_VALUE_1S_1_PRESCALLAR, Bit23, TIMER_CONTROL_PRESCALLAR_1);
+
     frameBufferSetup(1024, 768, 16);
     printf(test);
     char n = '\n';
@@ -35,13 +40,16 @@ void kernel_main()
     while (1)
     {
 
-        while (1)
-        {
-            if (AUX_MU_LSR_REG & 0x01)
-                break;
-        }
-        char x = (AUX_MU_IO_REG);
-        printf("Hex value: %x     \t", x);
-        printf("Char : %c\n", x);
-    }
+        /*Following block polls the UART registers to check for input and prints them.
+        * Do not use this unless UART interrupt controlled input is not working.
+        */        
+    //     while (1)
+    //     {
+    //         if (AUX_MU_LSR_REG & 0x01)
+    //             break;
+    //     }
+    //     char x = (AUX_MU_IO_REG);
+    //     printf("Hex value: %x     \t", x);
+    //     printf("Char : %c\n", x);
+    // }
 }
